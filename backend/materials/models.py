b7c61671fe969ad5669e5963ef33b08451af2a1d
Constantine Putilov <putilov.constantine@yandex.ru>
2024-04-07 19:33:44 +0500

    feat(materials): Добавлены поля short_description и image к модели Material

--- backend/materials/models.py
+++ backend/materials/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 from django_ckeditor_5.fields import CKEditor5Field
 
+from common.models import MultiImageMeta, Spec
 from sections.models import Section
 
 
-class Material(models.Model):
+class Material(models.Model, metaclass=MultiImageMeta):
+    IMAGE_WIDTH = 1024
+    IMAGE_HEIGHT = 768
+
     section = models.ForeignKey(
         Section,
         verbose_name="Раздел",
@@ -23,13 +27,29 @@ class Material(models.Model):
         verbose_name="Заголовок",
         max_length=256
     )
+    short_description = models.TextField(
+        verbose_name="Короткое описание",
+        max_length=568,
+        blank=True,
+        null=True,
+    )
     text = CKEditor5Field(
         verbose_name="Текст материала",
         blank=True,
         null=True,
     )
+    image = models.ImageField(
+        verbose_name="Превью",
+        upload_to="materials/images",
+        blank=True,
+        null=True,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    image_map = {
+        "image_display": Spec("image", IMAGE_WIDTH, IMAGE_HEIGHT),
+    }
 
     def __str__(self):
         return f"{self.title} | {self.author} | {self.section}"
--- backend/materials/serializers.py
+++ backend/materials/serializers.py
@@ -1,16 +1,21 @@
 from rest_framework import serializers
 
+from common.rest_fields import MultiImageField
 from materials.models import Material
 
 
 class MaterialListSerializer(serializers.ModelSerializer):
     """Сериализатор списка материалов"""
 
+    image_display = MultiImageField(required=False, read_only=True)
+
     class Meta:
         model = Material
         fields = (
             "id",
             "title",
+            "short_description",
+            "image_display",
             "author",
             "section",
             "created_at",
