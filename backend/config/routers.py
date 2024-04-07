a3635fa80122e0d979b37a76e042e109859472af
Constantine Putilov <putilov.constantine@yandex.ru>
2024-04-07 11:14:42 +0500

    feat(lessons): Добавлены эндпоинты для уроков

--- backend/config/routers.py
+++ backend/config/routers.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 from users.viewsets import UserViewSet, UserProfileViewSet
 from sections.viewsets import SectionViewSet
 from materials.viewsets import MaterialViewSet
+from lessons.viewsets import LessonViewSet
 
 if settings.API_DOCS_ENABLE:
     router = DefaultRouter()
@@ -14,3 +15,4 @@ router.register("users", UserViewSet, basename="users")
 router.register("sections", SectionViewSet, basename="sections")
 router.register("materials", MaterialViewSet, basename="materials")
 router.register("profiles", UserProfileViewSet, basename="profiles")
+router.register("lessons", LessonViewSet, basename="lessons")
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "users.apps.UsersConfig",
     "sections.apps.SectionsConfig",
     "materials.apps.MaterialsConfig",
+    "lessons.apps.LessonsConfig",
 ]
 
 MIDDLEWARE = [
